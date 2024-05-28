a21a0375095b648538c97d7babea773084fc83ea
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-28 13:29:14 -0300

    calculando corretamente o imc e exibindo corretamente na tela

--- js/principal.js
+++ js/principal.js
@@ -6,6 +6,8 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = (peso / altura * altura); // 100 / 2.0 x 2.0 = 100 / 4 =>>>>>>> 25
+var tdImc = paciente.querySelector(".info-imc");
 
-console.log(imc);
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;

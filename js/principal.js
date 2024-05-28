62c608e9f10076746f02e48f60c80a1a9b67ed12
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-28 13:46:32 -0300

    Operadores Lógicos

--- js/principal.js
+++ js/principal.js
@@ -8,6 +8,22 @@ var altura = tdAltura.textContent;
 
 var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso / (altura * altura);
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-tdImc.textContent = imc;
+if (peso <= 0 || peso >= 1000) {
+    console.log('peso invalido');
+    pesoEhValido = false;
+    tdImc.textContent = "Peso invalido!";
+}
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("altura invalida");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura invalida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}

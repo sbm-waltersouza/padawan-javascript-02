9affe2953af67a50e8308f42669a87b496bdc933
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-05-29 15:07:12 -0300

    Animando a remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,11 +1,10 @@
-var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("#tabela-pacientes");
 
-var tabela = document.querySelector("table");
+tabela.addEventListener("dblclick", function(event) {
+    event.target.parentNode.classList.add("fadeOut");
 
-tabela.addEventListener("dblclick", function() {
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode;
-    
-    paiDoAlvo.remove();
+    setTimeout(function() {
+        event.target.parentNode.remove();
+    }, 500);
 
 });
